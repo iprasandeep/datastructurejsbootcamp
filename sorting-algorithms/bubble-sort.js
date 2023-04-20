171fc89f7140e83c1e6a45384a9959b07a753c6f
Prasandeep <prasandeepp@gmail.com>
2023-04-20 22:33:32 +0530

    bubble sort completed!

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -4,3 +4,44 @@
 // ex: 29, 10, 14, 30, 37, 14, 18
 
 
+// Pseudocode
+
+/*
+- Start looping from with a variable called i the end of the array towards the beginning
+- start an inner loop with a variable caled j from the begining until i-1
+- if arr[j] is greater than arr[j+1], swap those two values!
+- return the sorted array
+*/
+
+// implementation
+
+function bubbleSort(arr){
+    for(let i = arr.length; i>0; i--)
+    {
+        let noswaps;
+        for(let j = 0; j<i-1; j++)
+        {
+            noswaps = true;
+            if(arr[j]> arr[j+1])
+            {
+                let temp  = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noswaps = false;
+            }
+        }
+    }
+    return arr;
+}
+let nums = [37, 45, 29, 8];
+let ans = bubbleSort(nums);
+console.log(ans);
+
+
+
+/*
+
+Time Complexity: O(N^2)
+Space: O(1)
+
+*/ 
